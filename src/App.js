6fae8aa2885d75afade135b98e4fa868c3e33471
sefiamor <48059153+sefiamor@users.noreply.github.com>
2022-10-28 17:10:34 -0700

    9. React Router

--- src/App.js
+++ src/App.js
@@ -3,13 +3,21 @@ import GlobalStyle from './components/GlobalStyle';
 //Import Pages
 import AboutUs from "./pages/AboutUs";
 import Nav from "./components/Nav";
+import ContactUs from "./pages/ContactUs";
+import OurWork from "./pages/OurWork";
+// Router
+import {Routes, Route} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
       <Nav />
-      <AboutUs />
+      <Routes>
+        <Route path="/" exact element={<AboutUs />} />
+        <Route path='/work' element={<OurWork />} />
+        <Route path='/contact' element={<ContactUs />}/>
+      </Routes>
     </div>
   );
 }
